7b9fecc62efe6b2e27fd62d30d4978a4f83a7b5e
Riflusso <vadim2005110@gmail.com>
2025-04-04 09:58:26 +0400

    Fix compatability with sodium 0.6.13

--- src/main/java/me/cortex/nvidium/mixin/sodium/MixinPerformanceSettings.java
+++ src/main/java/me/cortex/nvidium/mixin/sodium/MixinPerformanceSettings.java
@@ -9,7 +9,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(value = SodiumGameOptions.PerformanceSettings.class, remap = false)
+@Mixin(value = SodiumGameOptions.DebugSettings.class, remap = false)
 public class MixinPerformanceSettings {
     @Inject(method = "getSortBehavior()Lnet/caffeinemc/mods/sodium/client/render/chunk/translucent_sorting/SortBehavior;", at = @At(value = "HEAD"), cancellable = true)
     public void getSortBehavior(CallbackInfoReturnable<SortBehavior> cir) {
