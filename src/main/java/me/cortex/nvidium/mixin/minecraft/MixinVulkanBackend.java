9a3ae9f90e44f86e6ec17c45a89ca91b977d1eb4
drouarb <benjamin.drouard@epitech.eu>
2026-07-25 21:32:02 +0200

    sparse features

--- src/main/java/me/cortex/nvidium/mixin/minecraft/MixinVulkanBackend.java
+++ src/main/java/me/cortex/nvidium/mixin/minecraft/MixinVulkanBackend.java
@@ -45,6 +45,8 @@ public class MixinVulkanBackend {
     @Unique
     private static final Set<VulkanFeature> NVIDIUM_REQUIRED_FEATURES = Set.of(
             new VulkanFeature(VulkanBackend.VK10_FEATURES_STRUCT, "shaderInt64", VkPhysicalDeviceFeatures.SHADERINT64),
+            new VulkanFeature(VulkanBackend.VK10_FEATURES_STRUCT, "sparseBinding", VkPhysicalDeviceFeatures.SPARSEBINDING),
+            new VulkanFeature(VulkanBackend.VK10_FEATURES_STRUCT, "sparseResidencyBuffer", VkPhysicalDeviceFeatures.SPARSERESIDENCYBUFFER),
             new VulkanFeature(VulkanBackend.VK11_FEATURES_STRUCT, "storageBuffer16BitAccess", VkPhysicalDeviceVulkan11Features.STORAGEBUFFER16BITACCESS),
             new VulkanFeature(VulkanBackend.VK11_FEATURES_STRUCT, "uniformAndStorageBuffer16BitAccess", VkPhysicalDeviceVulkan11Features.UNIFORMANDSTORAGEBUFFER16BITACCESS),
             new VulkanFeature(VulkanBackend.VK12_FEATURES_STRUCT, "shaderInt8", VkPhysicalDeviceVulkan12Features.SHADERINT8),
